da2b803e06d7245b6a65b897623c05b248611977
VadymMurzin <prometey736@gmail.com>
2023-03-02 22:54:58 +0100

    example js

---
+++ script.js
@@ -0,0 +1,41 @@
+// let quantity = prompt("Choose quantity");
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// let width = '60px';
+// width = Number.parseInt(width);
+// console.log('width =', width, ';');
+
+// let salary = 1456.45678;
+// salary = salary.toFixed(2);
+// salary = Number.parseFloat(salary)
+
+// salary = Number.parseFloat(salary.toFixed(2))
+
+// console.log(salary);
+
+// let a = prompt("Введи число");
+// a = Number(a);
+// let b = prompt("Введи ступінь");
+// b = Number(b);
+// let c = Math.pow(a, b);
+// console.log(c);
+
+// const max = 70;
+// const min = 20;
+
+// const result = Math.round(Math.random() * (max - min) + min);
+
+// console.log(result);
+
+const color = ['red', 'green', 'blue', 'yellow', 'grey', 'sky'];
+const max = color.length - 1;
+const min = 0;
+
+const index = Math.round(Math.random() * (max - min) + min);
+const bgcolor = color[index];
+
+console.log(bgcolor);
+
+document.body.style.backgroundColor = bgcolor;
